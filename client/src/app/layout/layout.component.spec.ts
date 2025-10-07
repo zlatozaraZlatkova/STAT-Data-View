2f018f8987c4391124cdae50975aec542976c2b5
Zara <zlatozara@gmail.com>
2025-10-07 17:19:10 +0200

    test(layout): add route data handling tests

--- client/src/app/layout/layout.component.spec.ts
+++ client/src/app/layout/layout.component.spec.ts
@@ -1,18 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { Title } from '@angular/platform-browser';
+import { Subject } from 'rxjs';
 
 import { LayoutComponent } from './layout.component';
-import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
-import { RouterTestingModule } from '@angular/router/testing';
 import { HeaderComponent } from '../core/header/header.component';
 import { SelectMenuComponent } from '../shared/select-menu/select-menu.component';
 import { FooterComponent } from '../core/footer/footer.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MatIconModule } from '@angular/material/icon';
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
   let fixture: ComponentFixture<LayoutComponent>;
+
+  let router: Router;
+  let titleService: Title;
+  let mockActivatedRoute: ActivatedRoute;
+  let routerEventsSubject: Subject<any>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -20,42 +26,92 @@ describe('LayoutComponent', () => {
         LayoutComponent,
         HeaderComponent,
         FooterComponent,
-        SelectMenuComponent,
+        SelectMenuComponent
       ],
-      imports: [RouterTestingModule, HttpClientTestingModule, MatIconModule],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MatIconModule
+      ],
       providers: [
         {
           provide: ActivatedRoute,
           useValue: {
-            snapshot: { params: {}, queryParams: {} },
-            paramMap: of(),
-            queryParamMap: of(),
+            snapshot: {
+              params: {},
+              queryParams: {},
+              data: {}
+            },
+            firstChild: null
           },
         },
       ],
     });
+
     fixture = TestBed.createComponent(LayoutComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    // configureTestingModule
+    router = TestBed.inject(Router);
+    titleService = TestBed.inject(Title);
+    mockActivatedRoute = TestBed.inject(ActivatedRoute);
+
+    // mock Router events
+    routerEventsSubject = new Subject();
+    spyOnProperty(router, 'events', 'get').and.returnValue(routerEventsSubject.asObservable());
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('UI Rendering', () => {
+  describe('Route Data Handling', () => {
 
-    it('should always render main element', () => {
+    it('should set page title from route data', fakeAsync(() => {
+      const titleSpy = spyOn(titleService, 'setTitle');
+      mockActivatedRoute.snapshot.data = { title: 'Test Page' };
+
       fixture.detectChanges();
 
+      routerEventsSubject.next(new NavigationEnd(1, '/test', '/test'));
+      tick();
+
+      expect(titleSpy).toHaveBeenCalledWith('Test Page');
+    }));
+
+    it('should hide header and footer on 404 page', fakeAsync(() => {
+      mockActivatedRoute.snapshot.data = {
+        showHeader: false,
+        showFooter: false
+      };
+
+      component.ngOnInit();
+
+      routerEventsSubject.next(new NavigationEnd(1, '/page-not-found', '/page-not-found'));
+      tick();
+
+      expect(component.showHeader).toBe(false);
+      expect(component.showFooter).toBe(false);
+
+      fixture.detectChanges();
+      const header = fixture.nativeElement.querySelector('app-header');
+      const footer = fixture.nativeElement.querySelector('app-footer');
+
+      expect(header).toBeFalsy();
+      expect(footer).toBeFalsy();
+    }));
+  });
+
+  describe('UI Rendering', () => {
+    it('should always render main element', () => {
+      fixture.detectChanges();
       const main = fixture.nativeElement.querySelector('main');
       expect(main).toBeTruthy();
     });
-    
 
     it('should always render router-outlet', () => {
+      fixture.detectChanges();
       const routerOutlet = fixture.nativeElement.querySelector('router-outlet');
-      
       expect(routerOutlet).toBeTruthy();
     });
 
@@ -82,7 +138,6 @@ describe('LayoutComponent', () => {
 
     it('should hide header when showHeader is false', () => {
       component.showHeader = false;
-
       fixture.detectChanges();
 
       const header = fixture.nativeElement.querySelector('app-header');
@@ -93,7 +148,6 @@ describe('LayoutComponent', () => {
   describe('Footer Visibility', () => {
     it('should show footer when showFooter is true', () => {
       component.showFooter = true;
-
       fixture.detectChanges();
 
       const footer = fixture.nativeElement.querySelector('app-footer');
@@ -102,13 +156,10 @@ describe('LayoutComponent', () => {
 
     it('should hide footer when showFooter is false', () => {
       component.showFooter = false;
-
       fixture.detectChanges();
 
       const footer = fixture.nativeElement.querySelector('app-footer');
       expect(footer).toBeFalsy();
     });
-
   });
-
 });
