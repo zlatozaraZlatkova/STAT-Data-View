cc6d30b987e9e45a398897b623044da868e09147
Zara <zlatozara@gmail.com>
2025-10-07 15:49:53 +0200

    test(page-not-found): add unit tests for 404 error page

--- client/src/app/core/page-not-found/page-not-found.component.spec.ts
+++ client/src/app/core/page-not-found/page-not-found.component.spec.ts
@@ -8,14 +8,63 @@ describe('PageNotFoundComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PageNotFoundComponent]
+      declarations: [PageNotFoundComponent],
     });
     fixture = TestBed.createComponent(PageNotFoundComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  function getElByTestId(testId: string): HTMLElement {
+    return fixture.nativeElement.querySelector(`[data-testid="${testId}"]`);
+  }
+
+
+  describe('Display text at page 404', () => {
+    const elements = [
+      { testId: 'header-text', text: 'Page not found' },
+      {
+        testId: 'page-text',
+        text: `Sorry, we couldn't find the page you're looking for.`,
+      },
+    ];
+
+    it('should display correct text at all checked elements', () => {
+      fixture.detectChanges();
+
+      elements.forEach(({ testId, text }) => {
+        const currEl = getElByTestId(testId);
+
+        expect(currEl).withContext(`${testId} element`).toBeTruthy();
+
+        expect(currEl.textContent?.trim() ?? '')
+          .withContext(`${testId} text`)
+          .toBe(text);
+      });
+    });
+
+    it('should not display empty text', () => {
+      fixture.detectChanges();
+
+      const elements = [
+        { testId: 'header-text' },
+        { testId: 'page-text' }
+      ];
+
+      elements.forEach(({ testId }) => {
+        const currEl = getElByTestId(testId);
+
+        expect(currEl.textContent?.trim())
+          .withContext(`${testId} should not be empty`)
+          .not.toBe('');
+      });
+    });
+
+
+  });
+
 });
